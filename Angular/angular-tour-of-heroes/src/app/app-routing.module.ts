7cfac9d51eb8eee2dca42fc9328cad68af01c1a6
huuhien1747 <huuhienlee1234@gmail.com>
2019-03-21 13:48:24 +0700

    Buil Successful

--- Angular/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ Angular/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,3 +1,28 @@
+//============================ V1
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+
+//list of routes
+const routes: Routes = [
+
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
+
+
+/*
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -8,3 +33,7 @@ import { CommonModule } from '@angular/common';
   ]
 })
 export class AppRoutingModule { }
+*/ //====================================== V0
+
+
+
---
+++ Angular/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,44 @@
+
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+ 
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
+})
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+ 
+  constructor(private heroService: HeroService) { }
+ 
+  ngOnInit() {
+    this.getHeroes();
+  }
+  
+  //get heroes, return only four of the Top Heroes
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
+
+
+
+/*
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+*/
